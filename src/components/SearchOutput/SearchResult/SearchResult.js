aa6f1bee5b828f91fc87ab467b56c751835bb613
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2024-09-08 11:07:51 -0700

    finished level 7 front end

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -2,19 +2,17 @@ import React from "react";
 import classes from './SearchResult.module.css';
 
 const SearchResult = (props) => {
-    const {Phone, Location, Type, Carrier, Valid} = props
+    const {Phone, Valid, Location, Type, Carrier} = props
     console.log(props);
     return (
         <article className={classes.Result}>
             <h1>Phone Number: {Phone}</h1>
             <h3 className={classes.Result}>
 
+                    <p>Valid: {Valid[0]+""}</p>
                     <p>Location: {Location[0]}</p>
                     <p>Type: {Type[0]}</p>
                     <p>Carrier: {Carrier[0]}</p>
-                    <p>Valid: {Valid}</p>
-
-
 
             </h3>
 
